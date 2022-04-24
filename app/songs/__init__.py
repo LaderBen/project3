6fcac4513cc05385b272ca367a5790201a114d06
laderben <z874154748@sina.com>
2022-04-24 18:17:41 -0400

    fixed file upload function and modified the song model.

--- app/songs/__init__.py
+++ app/songs/__init__.py
@@ -32,16 +32,21 @@ def songs_upload():
     form = csv_upload()
     if form.validate_on_submit():
         log = logging.getLogger("myApp")
+        log.info('csv file uploaded')
 
         filename = secure_filename(form.file.data.filename)
         filepath = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
+        if not os.path.exists(current_app.config['UPLOAD_FOLDER']):
+            os.mkdir(current_app.config['UPLOAD_FOLDER'])
+        print(form.file)
         form.file.data.save(filepath)
+
         #user = current_user
         list_of_songs = []
         with open(filepath) as file:
             csv_file = csv.DictReader(file)
             for row in csv_file:
-                list_of_songs.append(Song(row['Name'],row['Artist']))
+                list_of_songs.append(Song(row['Name'],row['Artist'], row['Year'], row['Genre']))
 
         current_user.songs = list_of_songs
         db.session.commit()
