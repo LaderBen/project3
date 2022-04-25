dc619187e5ac180693c6326355dd9371503f8d60
laderben <z874154748@sina.com>
2022-04-25 14:21:46 -0400

    add test user test

--- app/config.py
+++ app/config.py
@@ -8,8 +8,9 @@ class Config(object):
     SECRET_KEY = 'This is an INSECURE secret!! DO NOT use this in production!!'
     SESSION_COOKIE_SECURE = True
     BOOTSTRAP_BOOTSWATCH_THEME = 'Simplex'
-    db_dir = "database/db.sqlite"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.abspath(db_dir)
+    # db_dir = "database/db.sqlite"
+    DB_DIR = os.getenv('DB_DIR', 'database')
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, '..', DB_DIR, "db2.sqlite")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER', BASE_DIR + '/uploads')
 
@@ -25,6 +26,6 @@ class DevelopmentConfig(Config):
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
     SESSION_COOKIE_SECURE = False
     DEBUG = True
