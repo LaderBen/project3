c49e0afcb886d324acf4e822f25a70fc0ad036a7
laderben <z874154748@sina.com>
2022-04-27 23:28:43 -0400

    add file upload test case

--- tests/login_test.py
+++ tests/login_test.py
@@ -1,4 +1,9 @@
 from app.db.models import User, Song
+from app import config
+
+from pathlib import Path
+import os
+import time
 from flask_login import current_user
 
 def test_register_user_function(client):
@@ -26,6 +31,7 @@ def test_user_login_function(client):
         'csrf_token': token
     }
     client.post('/register', data=data)
+    #login
     # get csrf token
     token = get_token('/login', client)
 
@@ -38,6 +44,53 @@ def test_user_login_function(client):
     #once login success it will redirect to /dashboard
     assert response.status_code == 302
     assert b'/dashboard' in response.data
+
+def test_user_file_upload(client):
+    # register a user
+    # get csrf token
+    token = get_token('/register', client)
+    data = {
+        'email': 'test@test.com',
+        'password': '123456',
+        'confirm': '123456',
+        'csrf_token': token
+    }
+    client.post('/register', data=data)
+    # login
+    # get csrf token
+    token = get_token('/login', client)
+
+    data = {
+        'email': 'test@test.com',
+        'password': '123456',
+        'csrf_token': token
+    }
+    client.post('/login', data=data)
+    #upload file
+    root = Path(__file__).parent.parent
+    test_file = root / 'tests'/'music.csv'
+    upload_dir = root / 'app' / 'uploads' / 'music.csv'
+    if os.path.exists(upload_dir):
+        os.remove(upload_dir)
+    assert not os.path.exists(upload_dir)
+    assert os.path.exists(test_file)
+    token = str(client.get('/songs/upload').data)
+    start = token.find('name="csrf_token" type="hidden" value="') + len('name="csrf_token" type="hidden" value="')
+    end = token.find(
+        '">\\n        \\n            \\n\\n    \\n    \\n        \\n    \\n\\n    \\n\\n    \\n    \\n    \\n    \\n    \\n\\n    <div class="mb-3 required"><label class="form-label" for="file">File</label>')
+    token = token[start:end]
+    data = {
+        'csrf_token': token,
+        'file': test_file.open('rb')
+    }
+    client.post('/songs/upload',data = data)
+    time.sleep(1)
+    upload_dir = root / 'app' / 'uploads' / 'home_myuser_tests_music.csv'
+    assert os.path.exists(upload_dir)
+    os.remove(upload_dir)
+
+
+
 
 def get_token(url, client):
     token = str(client.get(url).data)
